47c219e5c570a56405ea9720819ce97ba08c9e42
qingbing <780042175@qq.com>
2021-03-24 21:42:02 +0800

    format

--- doc/helpers/Format.md
+++ doc/helpers/Format.md
@@ -1,9 +1,16 @@
 # 助手类 Format ： 常用格式化
+- 获取当前纳米级时间表示 ： microDatetime()
 - 返回数组的值 ： dataValue(string $key, $data = [])
 - 标准格式化日期 ： date($time = null, $todayIfNull = true, $format = 'Y-m-d')
 - 标准格式化日期 ： datetime($time = null, $nowIfNull = true, $format = 'Y-m-d H:i:s')
-- 获取当前纳米级时间表示 ： microDatetime()
-
+- 将浮点数保留 $precision 位小数 ： round(float $val, $precision = 2)
+- 将浮点数转换成百分数表示 ： percent(float $val, $precision = 2)
+- 用户名 隐秘显示 ： maskUsername(string $username)
+- 姓名 隐秘显示 ： maskName(string $username)
+- 隐秘身份证中的年月日 ： maskIdentity(string $identity)
+- 手机号 隐秘显示 ： maskPhone(string $phone, $type = 0)
+- 银行卡 隐秘显示 ： maskBankCard(string $cardNum, int $type = 0)
+    
 ## test 代码
 
 ```php
@@ -15,14 +22,67 @@ var_dump(Format::date(time() - 86400 * 2));
 // 时间格式化
 var_dump(Format::datetime(time() - 86400 * 2));
 var_dump(Format::microDateTime());
+
+var_dump('=========');
+var_dump(Format::round(12));
+var_dump(Format::round(.0911));
+var_dump(Format::round(12.0000, 6));
+var_dump(Format::round(12.9999, 3));
+var_dump(Format::round('12.9299'));
+var_dump(Format::round(12.9219));
+
+var_dump('=========');
+var_dump(Format::percent(12));
+var_dump(Format::percent(.0911));
+var_dump(Format::percent(12.0000, 6));
+var_dump(Format::percent(12.9999, 3));
+var_dump(Format::percent('12.9299'));
+var_dump(Format::percent(12.9219));
+
+var_dump('=========');
+var_dump(Format::maskUsername("qingbing1"));
+var_dump(Format::maskUsername("杜小冰"));
+var_dump(Format::maskName("杜小冰"));
+var_dump(Format::maskIdentity("511623198803141189"));
+var_dump(Format::maskPhone("13608051299"));
+var_dump(Format::maskPhone("13608051299", 1));
+var_dump(Format::maskPhone("13608051299", 2));
+var_dump(Format::maskBankCard("123456789012345678"));
+var_dump(Format::maskBankCard("123456789012345678", 1));
 ```
 
 ## test 结果
 
 ```php
+
 string(8) "qingbing"
 string(10) "2021-03-24"
 string(10) "2021-03-22"
-string(19) "2021-03-22 02:34:14"
-string(26) "2021-03-24 02:34:14.917199"
+string(19) "2021-03-22 13:38:41"
+string(26) "2021-03-24 13:38:41.785062"
+string(9) "========="
+string(5) "12.00"
+string(4) "0.09"
+string(9) "12.000000"
+string(6) "13.000"
+string(5) "12.93"
+string(5) "12.92"
+string(9) "========="
+string(8) "1200.00%"
+string(5) "9.11%"
+string(12) "1200.000000%"
+string(9) "1299.990%"
+string(8) "1292.99%"
+string(8) "1292.19%"
+string(9) "========="
+string(9) "qingb****"
+string(7) "杜小*"
+string(5) "杜**"
+string(18) "511623********1189"
+string(11) "136****1299"
+string(8) "****1299"
+string(10) "尾号1299"
+string(19) "1234 **** **** 5678"
+string(18) "1234**********5678"
+
 ```
--- src/helpers/Format.php
+++ src/helpers/Format.php
@@ -20,6 +20,35 @@ use Exception;
 class Format
 {
     /**
+     * 时区对象
+     *
+     * @var DateTimeZone
+     */
+    private static $timezone;
+
+    /**
+     * 获取当前纳米级时间表示
+     *
+     * @return string
+     * @throws Exception
+     */
+    public static function microDatetime()
+    {
+        // 确保时区是设置的
+        if (!self::$timezone) {
+            self::$timezone = new DateTimeZone(date_default_timezone_get() ?: 'PRC');
+        }
+        // php7.1+ 始终开启的纳秒
+        if (PHP_VERSION_ID < 70100) {
+            $ts = DateTime::createFromFormat('U.u', sprintf('%.6F', microtime(true)), self::$timezone);
+        } else {
+            $ts = new DateTime(null, self::$timezone);
+        }
+        $ts->setTimezone(self::$timezone);
+        return $ts->format('Y-m-d H:i:s.u');
+    }
+
+    /**
      * 返回数组的值
      *
      * @param string $key
@@ -69,31 +98,97 @@ class Format
     }
 
     /**
-     * 时区对象
-     *
-     * @var DateTimeZone
+     * 将浮点数保留 $precision 位小数
+     * @param float $val
+     * @param int $precision
+     * @return string
      */
-    private static $timezone;
+    public static function round(float $val, $precision = 2)
+    {
+        return sprintf("%.{$precision}f", $val);
+    }
 
     /**
-     * 获取当前纳米级时间表示
+     * 将浮点数转换成百分数表示
      *
+     * @param float $val
+     * @param int $precision
      * @return string
-     * @throws Exception
      */
-    public static function microDatetime()
+    public static function percent(float $val, $precision = 2)
     {
-        // 确保时区是设置的
-        if (!self::$timezone) {
-            self::$timezone = new DateTimeZone(date_default_timezone_get() ?: 'PRC');
+        return self::round($val * 100, $precision) . '%';
+    }
+
+    /**
+     * 用户名 隐秘显示
+     * @param string $username
+     * @return string
+     */
+    public static function maskUsername(string $username)
+    {
+        $len       = mb_strlen($username);
+        $repeatLen = ceil($len / 2);
+        $repeat    = str_repeat('*', $len - $repeatLen);
+        return mb_substr($username, 0, $repeatLen) . $repeat;
+    }
+
+    /**
+     * 姓名 隐秘显示
+     * @param string $username
+     * @return string
+     */
+    public static function maskName(string $username)
+    {
+        $repeat = str_repeat('*', mb_strlen($username) - 1);
+        return mb_substr($username, 0, 1) . $repeat;
+    }
+
+    /**
+     * 隐秘身份证中的年月日
+     * @param string $identity
+     * @return mixed
+     */
+    public static function maskIdentity(string $identity)
+    {
+        if (strlen($identity) == 18) {
+            return substr_replace($identity, '********', 6, 8);
         }
-        // php7.1+ 始终开启的纳秒
-        if (PHP_VERSION_ID < 70100) {
-            $ts = DateTime::createFromFormat('U.u', sprintf('%.6F', microtime(true)), self::$timezone);
-        } else {
-            $ts = new DateTime(null, self::$timezone);
+        return substr_replace($identity, '********', 4, 8);
+    }
+
+    /**
+     * 手机号 隐秘显示
+     *
+     * @param string $phone
+     * @param int $type 安全类型
+     * @return mixed|string
+     */
+    public static function maskPhone(string $phone, $type = 0)
+    {
+        if ($type == 1) { //仅仅显示后面四位
+            return '****' . substr($phone, -4);
         }
-        $ts->setTimezone(self::$timezone);
-        return $ts->format('Y-m-d H:i:s.u');
+        if ($type == 2) {
+            return '尾号' . substr($phone, -4);
+        }
+        //屏蔽中间四位
+        return substr_replace($phone, '****', 3, 4);
+    }
+
+    /**
+     * 银行卡 隐秘显示
+     *
+     * @param string $cardNum
+     * @param int $type
+     * @return mixed
+     */
+    public static function maskBankCard(string $cardNum, int $type = 0)
+    {
+        if (1 === $type) {
+            $len = strlen($cardNum);
+            return substr_replace($cardNum, str_repeat('*', $len - 8), 4, -4);
+        }
+        return substr_replace($cardNum, ' **** **** ', 4, -4);
     }
 }
